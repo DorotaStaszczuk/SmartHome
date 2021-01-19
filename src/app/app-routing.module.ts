a74313bbd1c5231d00592f264b58d4b07c35424b
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-01-19 18:46:18 +0100

    two paths added: main and room

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { RoomContentComponent } from './components/room-content/room-content.component';
+import { MainComponent } from './components/main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'room', component: RoomContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
