68e47211ab71917d934482a5b1b6e46b2efe1818
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-01-20 19:46:07 +0100

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './components/main/main.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { RoomCoverComponent } from './components/room-cover/room-cover.component';
+import { RoomContentComponent } from './components/room-content/room-content.component';
+import { RoomHeaderComponent } from './components/room-header/room-header.component';
+import { ObjectComponent } from './components/object/object.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MainHeaderComponent,
+    RoomCoverComponent,
+    RoomContentComponent,
+    RoomHeaderComponent,
+    ObjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
