493119cdcf443d704f689da5f7afca8955d7a8fe
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-01-21 19:41:30 +0100

    firebase config added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrp59njl4b9pvKBDmlCnQDO3Sm0-BTdfg",
+    authDomain: "smarthome-b53e5.firebaseapp.com",
+    databaseURL: "https://smarthome-b53e5-default-rtdb.firebaseio.com",
+    projectId: "smarthome-b53e5",
+    storageBucket: "smarthome-b53e5.appspot.com",
+    messagingSenderId: "286443228325"
+  }
 };
