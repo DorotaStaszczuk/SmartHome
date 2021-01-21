2f17ef679668b390314936a3a326b08e3f7d306c
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2021-01-21 14:38:40 +0100

    AngularFireModule.initializeApp(environment.firebase),

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { MainComponent } from './components/main/main.component';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
 import { RoomCoverComponent } from './components/room-cover/room-cover.component';
@@ -12,6 +15,8 @@ import { ObjectComponent } from './components/object/object.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +33,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     BrowserAnimationsModule,
     MatSliderModule,
     MatSlideToggleModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
